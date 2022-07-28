ea8f5854fc9c27a6e1d2cc7ac8b89534a4db20d0
Alexander Whitlock <zanderwhitlock@gmail.com>
2022-07-28 14:28:10 +0300

    first commit

---
+++ fcrg-webapp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fcrg-root',
+  templateUrl: './app.component.html',
+  styles: []
+})
+export class AppComponent {
+  title = 'fcrg-webapp';
+}
